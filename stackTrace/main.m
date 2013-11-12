11ab2f398dfacb371e35a65545d280258959fc33
Zulkarnyaev Azat <vmkkfu@gmail.com>
2013-11-12 14:01:41 +0400

    Initial Commit

---
+++ stackTrace/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  stackTrace
+//
+//  Created by Азат Зулькарняев on 12.11.13.
+//  Copyright (c) 2013 Азат Зулькарняев. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
